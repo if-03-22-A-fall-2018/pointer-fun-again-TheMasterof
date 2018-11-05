ec5d8d65ce4fb471ffd9632ec01a4cd708ebb7c0
Marcel Plakolb <marcel.plakolb@liwest.at>
2018-11-05 15:57:42 +0100

    added 2. and 3.

---
+++ pointer_fun_again.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+struct Playstruct{
+  int int_value;
+  double double_value;
+  char a_string[64];
+}
+int main(int argc, char const *argv[]) {
+  struct Playstruct play_struct
+  return 0;
+}
